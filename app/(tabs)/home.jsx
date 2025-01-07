225347c6c616559bb2655cac52018cfd5c093d35
Your Username <ubaidullah2305@gmail.com>
2025-01-07 16:25:25 +0500

    show category section and fetch from db

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,7 +2,7 @@ import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import Header from "@/components/Home/Header";
 import Slider from "@/components/Home/Slider";
-// import Category from "../../components/Home/Category";
+import Category from "@/components/Home/Category";
 // import PopularBusiness from "../../components/Home/PopularBusiness";
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <Slider />
 
       {/*Category*/}
-      {/* <Category /> */}
+      <Category />
 
       {/*Popular Business List*/}
       {/* <PopularBusiness /> */}
