adb9ca673b69664be445ca77a948fec8106a1abb
Your Username <ubaidullah2305@gmail.com>
2025-01-07 16:40:56 +0500

    list popular business , fetch by db

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "@/components/Home/Header";
 import Slider from "@/components/Home/Slider";
 import Category from "@/components/Home/Category";
-// import PopularBusiness from "../../components/Home/PopularBusiness";
+import PopularBusiness from "@/components/Home/PopularBusiness";
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
       <Category />
 
       {/*Popular Business List*/}
-      {/* <PopularBusiness /> */}
+      <PopularBusiness />
 
       <View style={{ height: 50 }}></View>
     </ScrollView>
