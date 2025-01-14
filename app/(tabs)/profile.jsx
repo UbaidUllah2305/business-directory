6a38ce7f4c3c6291b572586ca9160926152561d0
Your Username <ubaidullah2305@gmail.com>
2025-01-14 10:04:17 +0500

    profile page complete

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import UserIntro from "../../components/Profile/UserIntro";
+import MenuList from "../../components/Profile/MenuList";
 
-export default function profile() {
+export default function Profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View
+      style={{
+        padding: 20,
+        marginTop: 20,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 35,
+        }}
+      >
+        Profile
+      </Text>
+
+      {/* User Intro  */}
+      <UserIntro />
+
+      {/* Menu List  */}
+      <MenuList />
     </View>
-  )
+  );
 }
